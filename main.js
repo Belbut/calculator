dc4287b357c553901d9518c1f1ee060b3f574d17
Belbut <andreferreirabelbut@gmail.com>
2022-10-07 12:11:50 +0100

    Add basic math functions

---
+++ main.js
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------- ELements
+
+
+//----------------------------------------------------------------- Variables
+
+
+//----------------------------------------------------------------- Functions
+
+const add = (number1, number2) => number1 + number2;
+const subtract = (number1, number2) => number1 - number2;
+const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number2 === 0 ? "ERROR" : number1 / number2;
+const factorial = (number) => number === 0 ? 1 : number * factorial(number - 1);
+
+
+
+
+//----------------------------------------------------------------- Run
