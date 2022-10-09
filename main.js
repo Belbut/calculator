d9e6e621ebe1b9b350440154b56c5fea6ef4adab
Belbut <andreferreirabelbut@gmail.com>
2022-10-09 20:04:10 +0100

    Add numbers listeners and display math functions

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 //----------------------------------------------------------------- ELements
+const outputFunction = document.getElementById("output-function");
+const outputResult = document.getElementById("output-result");
+
+const inputNumbers = document.getElementsByClassName("number");
+
+//----------------------------------------------------------------- Listeners
+function numberListener() {
+    for (let number of inputNumbers) {
+        number.addEventListener("click", (clickedNumber) => {
+            if (clickedNumber.target.textContent == ".") {
+                if(usedDecimal) return;
+                usedDecimal = true;
+            }
+            calculationFunction += clickedNumber.target.textContent;
+            outputFunction.textContent = Number(calculationFunction);
+        })
+    }
+}
 
 
 //----------------------------------------------------------------- Variables
-
+let calculationResult = ""; // max numbers is 24
+let calculationFunction = "";
+let usedDecimal = false;
 
 //----------------------------------------------------------------- Functions
 
@@ -13,6 +33,7 @@ const divide = (number1, number2) => number2 === 0 ? "ERROR" : number1 / number2
 const factorial = (number) => number === 0 ? 1 : number * factorial(number - 1);
 
 
-
-
 //----------------------------------------------------------------- Run
+outputResult.textContent = calculationResult;
+
+numberListener();
