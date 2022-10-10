3c0a74c7d3cd43f2b556bdc64eab44d9a1450f7b
Belbut <andreferreirabelbut@gmail.com>
2022-10-10 15:09:05 +0100

    Add operate function

--- main.js
+++ main.js
@@ -3,26 +3,29 @@ const outputFunction = document.getElementById("output-function");
 const outputResult = document.getElementById("output-result");
 
 const inputNumbers = document.getElementsByClassName("number");
+const inputOperators = document.getElementsByClassName("operator");
 
 //----------------------------------------------------------------- Listeners
 function numberListener() {
     for (let number of inputNumbers) {
-        number.addEventListener("click", (clickedNumber) => {
-            if (clickedNumber.target.textContent == ".") {
-                if(usedDecimal) return;
+        number.addEventListener("click", () => {
+            if (number.textContent == ".") {
+                if (usedDecimal) return;
                 usedDecimal = true;
             }
-            calculationFunction += clickedNumber.target.textContent;
+            calculationFunction += number.textContent;
             outputFunction.textContent = Number(calculationFunction);
         })
     }
 }
 
 
+
 //----------------------------------------------------------------- Variables
 let calculationResult = ""; // max numbers is 24
 let calculationFunction = "";
 let usedDecimal = false;
+const OPERATORS = ["addition", "subtraction", "multiplication", "division", "factorial"];
 
 //----------------------------------------------------------------- Functions
 
@@ -32,8 +35,29 @@ const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => number2 === 0 ? "ERROR" : number1 / number2;
 const factorial = (number) => number === 0 ? 1 : number * factorial(number - 1);
 
+function operate(operator, number1, number2) {
+
+    switch (operator) {
+        case OPERATORS[0]:
+            return add(number1, number2);
+
+        case OPERATORS[1]:
+            return subtract(number1, number2);
+
+        case OPERATORS[2]:
+            return multiply(number1, number2);
+
+        case OPERATORS[3]:
+            return divide(number1, number2);
+
+        case OPERATORS[4]:
+            return factorial(number1);
+    }
+}
+
+
 
 //----------------------------------------------------------------- Run
 outputResult.textContent = calculationResult;
 
-numberListener();
+onStart();
